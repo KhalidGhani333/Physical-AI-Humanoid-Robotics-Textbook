925fb77dd8faffa0fabad18e638a67ac0f7561db
khalid ghani <khalidghani.333@gmail.com>
2025-12-10 19:02:09 +0500

    error resolve

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="/docs/introduction">
+              to="/docs/intro">
               Let's now Start
             </Link>
           </div>
