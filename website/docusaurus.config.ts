b4ce77162b710e16caf9a781ebedd7e27b269abd
Khalid Ghani <159691720+KhalidGhani333@users.noreply.github.com>
2025-12-26 16:32:21 +0500

    Update production URL in Docusaurus config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-github-username.github.io',
+  url: 'https://physical-ai-humanoid-robotics-textb-sooty-psi.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
